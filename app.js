f6c2a4884737ad8a75fae4d19350b959c3a9793d
Varghese1987 <61176244+Varghese1987@users.noreply.github.com>
2022-02-02 16:55:55 +0530

    cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require("http-errors");
 var dotenv = require("dotenv").config();
 var express = require("express");
+const cors = require("cors");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
@@ -42,6 +43,8 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
+
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
